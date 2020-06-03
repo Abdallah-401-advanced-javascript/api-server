bbace678ddd0267006f48de644a3d575e342e66e
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-03 23:55:34 +0300

    done

--- middleware/timestamp.js
+++ middleware/timestamp.js
@@ -3,6 +3,6 @@
 module.exports = (req, res, next) => {
   req.requestTime=new Date();
   // let requestTime=new Date();
-  // console.log(req.requestTime);
+  console.log(req.requestTime);
   next();
 };
